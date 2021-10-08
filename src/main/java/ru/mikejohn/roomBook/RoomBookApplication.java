6d3948563f9ad086fc31778e81a318ef33b54ba4
Glushkovskii <mikejohn@yandex.ru>
2021-10-08 09:46:51 +0300

    Init commit

---
+++ src/main/java/ru/mikejohn/roomBook/RoomBookApplication.java
@@ -0,0 +1,13 @@
+package ru.mikejohn.roomBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mikejohn/roomBook/RoomBookApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mikejohn.roomBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
